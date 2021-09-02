c2022007759108e7f93cece4a9b639242a62e3bf
Simon Koob <simon.koob@gmx.net>
2021-09-02 20:09:59 +0200

    Timer for team event

---
+++ script.js
@@ -0,0 +1,34 @@
+const daysElement =document.getElementById('days');
+const hoursElement =document.getElementById('hours');
+const minutesElement =document.getElementById('minutes');
+const secondsElement =document.getElementById('seconds');
+
+const newYears = "15 Oct 2021, 12:00";
+
+function countdown () {
+    const currentDate =new Date();
+    const newYearsDate = new Date(newYears);
+
+    const totalSeconds =(newYearsDate - currentDate)/ 1000;
+
+    const days = Math.floor(totalSeconds / 3600 / 24); /* Math.floor sets full number */
+    const hours = Math.floor(((totalSeconds/3600) % 24)); /* Module for hours here, how much is left until 24 is full */
+    const minutes = Math.floor(totalSeconds/60) % 60; /* Module for minutess here, how much is left until 60 is full*/
+    const seconds = Math.floor(totalSeconds) % 60;                 /* How much until 60 left */
+
+    daysElement.innerHTML = days;
+    hoursElement.innerHTML = formatTime(hours);
+    minutesElement.innerHTML = formatTime(minutes);
+    secondsElement.innerHTML = formatTime(seconds);
+}
+
+function formatTime(time) {
+    return time < 10 ? (`0${time}`) : time;         /* if less than 10 time period remaining, show 0 infront */
+}
+
+//Initial call to get Miliseconds until New Years
+countdown();
+
+//get Seconds until New Years
+setInterval(countdown, 1000);
+
