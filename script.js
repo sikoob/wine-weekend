e88b292b22256de30b2c29360e0faf0b36326606
sikoob <simon.koob@gmx.net>
2023-08-29 19:23:46 +0200

    update 2023

--- script.js
+++ script.js
@@ -3,13 +3,15 @@ const hoursElement =document.getElementById('hours');
 const minutesElement =document.getElementById('minutes');
 const secondsElement =document.getElementById('seconds');
 
-const newDate = "15 Oct 2021, 15:00";
+const newDate = "13 Oct 2023, 17:00";
 
 function countdown () {
     const currentDate =new Date();
     const newDate2 = new Date(newDate);
 
     const totalSeconds =(newDate2 - currentDate)/ 1000;
+
+    if (totalSeconds >= 0) {
 
     const days = Math.floor(totalSeconds / 3600 / 24); /* Math.floor sets full number */
     const hours = Math.floor(((totalSeconds/3600) % 24)); /* Module for hours here, how much is left until 24 is full */
@@ -20,6 +22,13 @@ function countdown () {
     hoursElement.innerHTML = formatTime(hours);
     minutesElement.innerHTML = formatTime(minutes);
     secondsElement.innerHTML = formatTime(seconds);
+
+    } else {
+    daysElement.innerHTML = 0;
+    hoursElement.innerHTML = 0;
+    minutesElement.innerHTML = 0;
+    secondsElement.innerHTML = 0;
+    };
 }
 
 function formatTime(time) {
